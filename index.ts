2eaf7d1fbe2509765b80f4861d45c44eceb4341b
iqrausama97 <iqra10412@gmail.com>
2024-03-31 11:19:26 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer"
+
+let todolist = [];
+let loop = true;
+
+while(loop){
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message:"What you want to add in your Todo List."
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "fasle"
+        }
+    ]
+);
+todolist.push(addTask.todo);
+loop = addTask.addMore;
+console.log(todolist);
+}
